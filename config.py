55ff33f326bf45d451b70d43ef498c5de1c20514
BiriBiriBiriBiri <thatweeaboonerd@zoho.com>
2018-04-21 13:36:49 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-password = 'error' # Replace this with your discord password
 username = 'error' # Replace this with your discord username
+password = 'error' # Replace this with your discord password
